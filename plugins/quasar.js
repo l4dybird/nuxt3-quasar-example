216d0e5579c373c5240e6278be5b7d14de486aaf
Yuki <k.yuuki1997@gmail.com>
2022-06-27 22:37:50 +0900

    :recycle: update nuxt3 rc

--- plugins/quasar.js
+++ plugins/quasar.js
@@ -1,11 +1,13 @@
 import { Quasar } from 'quasar'
-import * as All from 'quasar'
+import * as components from 'quasar'
+import * as directives from 'quasar'
 import langJa from 'quasar/lang/ja'
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(Quasar, {
         config: {},
-        components: All,
+        components,
+        directives,
         plugins: {},
         // ここは Quasar 側の lang 設定なので好みです。
         lang: langJa
